71cd59604d94e82c0cbb8ac5014ef54ce9d5b362
Mug <nico.thouvenin13@gmail.com>
2018-08-07 15:55:58 +0200

    oui
    connect toussa

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,54 @@
 import React from "react"
 
-export default () => <div>Hellooooooooooooooo world!</div>
+// Get a reference to the database service
+//var database = firebase.database();
+var Rebase = require('re-base');
+var firebase = require('firebase');
+
+var config = {
+  apiKey: "AIzaSyD37czVcJGMg7Hzrxa9UMVcMEyhzr83Q-8",
+  authDomain: "firstprojectfb-4d6e4.firebaseapp.com",
+  databaseURL: "https://firstprojectfb-4d6e4.firebaseio.com",
+  projectId: "firstprojectfb-4d6e4",
+  storageBucket: "firstprojectfb-4d6e4.appspot.com",
+  messagingSenderId: "137088614221"
+};
+var app = firebase.initializeApp(config);
+var base = Rebase.createClass(app.database());
+                
+class Afficher extends React.Components{
+	constructor(props){
+		super(props);
+		this.state = {
+			text: '',
+		};
+	}
+
+	componentWillMount() {
+	    const { dispatch } = this.props
+	    base.listenTo('en', {
+	      context: this,
+	      then(copies) {
+	        dispatch(this.setState({
+	        	text: copies,
+	        }))
+	      },
+	      onFailure(error) {
+	        console.log('error', error)
+	      }
+	    })
+	}
+
+	render(){
+		return(
+			<div>
+				<div>{this.state.text}</div>
+			</div>
+		);
+	};
+}
+
+
+export default () => (
+	<Afficher/>
+);
